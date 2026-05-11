34b1b34976164069e985d2bef1309be306dc4fd8
h22j00 <hjjunghj0427@gmail.com>
2026-05-11 16:10:53 +0900

    sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 //  🔔 Service Worker — 백그라운드 푸시 알림
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-const CACHE_NAME = 'uniplus-pray-v3';
+const CACHE_NAME = 'uniplus-pray-v4';
 const URLS_TO_CACHE = [
   '/UNI-50/',
   '/UNI-50/index.html',
